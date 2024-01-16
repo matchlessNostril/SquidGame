c02b3ae7116ddf98dd3d5ac50065840b29149a76
Soru <144131324+matchlessNostril@users.noreply.github.com>
2024-01-17 01:44:07 +0900

    Added email-sign-in view & signIn feature

---
+++ squid-game/src/views/email-sign-in.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="flexCenter columnFlex">
+    <h1><b>E</b>mail <b>L</b>og<b>i</b>n</h1>
+    <!-- prevent : form의 기본 동작인 submit 후 새로 고침 방지 -->
+    <form @submit.prevent="signIn" class="flexCenter columnFlex">
+      <v-text-field
+        dark
+        color="#fe4063"
+        name="Email"
+        label="Email"
+        type="email"
+        v-model="email"
+        required
+      ></v-text-field>
+      <v-text-field
+        dark
+        color="#fe4063"
+        name="Password"
+        label="PassWord"
+        type="password"
+        v-model="password"
+        required
+      ></v-text-field>
+      <button type="submit" class="mt-5 btn squidRed">
+        <v-icon class="mr-2" color="white">mdi-login</v-icon>Sign In
+      </button>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    signIn() {
+      this.$store.dispatch("signIn", {
+        pEmail: this.email,
+        pPassword: this.password,
+      });
+    },
+  },
+};
+</script>
+<style src="../assets/css/button.css"></style>
+<style scoped src="../assets/css/form.css"></style>
